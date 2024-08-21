c61c9a07f02e781e71b325765ca52e95d8bf9c1e
Xinpeng Wang <223015056@link.cuhk.edu.cn>
2024-08-21 09:11:50 +0800

    1

--- main.py
+++ main.py
@@ -12,13 +12,25 @@ from rich.console import Console
 import torch
 from multiprocessing import cpu_count, Pool
 
+PROJECT_DIR = Path(__file__).parent.parent.parent.absolute()
+sys.path.append(PROJECT_DIR.as_posix())
+
 from data.partition_data import partition_and_statistic, get_partition_arguments, ALL_DOMAINS
 from algorithm.server.fedavg import FedAvgServer, get_fedavg_argparser
+from algorithm.server.fedprox import FedProxServer, get_fedprox_argparser
+from algorithm.server.fedsr import FedSRServer, get_fedsr_argparser
 from utils.tools import local_time
+
+algo2server = {"FedAvg": FedAvgServer, "FedProx": FedProxServer, "FedSR": FedSRServer}
+algo2argparser = {
+    "FedAvg": get_fedavg_argparser(),
+    "FedProx": get_fedprox_argparser(),
+    "FedSR": get_fedsr_argparser(),
+}
 
 
 def process(test_domain):
-    time.sleep(np.random.randint(0, 5))
+    time.sleep(np.random.randint(0, 10))
     # 1. partition data
     data_args = get_partition_arguments()
     data_args.test_domain = test_domain
@@ -26,28 +38,29 @@ def process(test_domain):
     data_args.directory_name = dir_name
     partition_and_statistic(deepcopy(data_args))
     # 2. train
-    fl_args = get_fedavg_argparser().parse_args()
+    fl_args = algo2argparser[algo].parse_args()
     fl_args.partition_info_dir = dir_name
     fl_args.output_dir = begin_time
-    server = FedAvgServer(args=deepcopy(fl_args))
+    server = algo2server[algo](args=deepcopy(fl_args))
     server.process_classification()
 
 
 def get_table():
     test_accuracy = {}
-    path2dir = os.path.join("out", "FedAvg", "pacs", begin_time)
+    path2dir = os.path.join("out", algo, "pacs", begin_time)
     for domain in domains:
         with open(os.path.join(path2dir, domain, "test_accuracy.pkl"), "rb") as f:
             test_accuracy[domain] = round(pickle.load(f), 2)
     average_accuracy = round(np.mean(list(test_accuracy.values())), 2)
     test_accuracy["average"] = average_accuracy
-    test_accuracy_df = pd.DataFrame(test_accuracy, index=["FedAvg"])
+    test_accuracy_df = pd.DataFrame(test_accuracy, index=[algo])
     test_accuracy_df.to_csv(os.path.join(path2dir, "test_accuracy.csv"))
     return test_accuracy
 
 
 if __name__ == "__main__":
     begin_time = local_time()
+    algo = "FedSR"
     domains = ["photo", "sketch", "art_painting", "cartoon"]
     multiprocess = True
     if multiprocess:
